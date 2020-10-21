1411b4b55e70a25e90a040b0eb3ace050135131b
Valerius <vvstouba@gmail.com>
2020-10-21 23:04:16 +0300

    fix: add towel sort logic

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let resultMatrix = [];
+  if(!matrix)
+    return resultMatrix;
+
+  let index = 0;
+  matrix.forEach(function(item, index) {
+    if(index % 2 == 0) {
+      for (var i = 0; i < item.length; i++) 
+        resultMatrix.push(item[i]);
+    }else {
+      for (var i = item.length -1; i >= 0; i--) 
+        resultMatrix.push(item[i]);
+    }
+  });
+  return resultMatrix;
 }
